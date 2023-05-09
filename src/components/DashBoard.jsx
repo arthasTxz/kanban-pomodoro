f62c8e14ce34e17e1a59114e0bfd5357fc92fc7f
Andre Caceres <elyabeto@gmail.com>
2023-05-08 20:26:52 -0400

    draggable semi resuelto

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ListTask from './ListTask'
 import ToggableList from './ToggableList'
-import { useState
+import { useState, useEffect
  } from "react";
- import { DndContext, closestCenter } from "@dnd-kit/core";
- import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
- import { useDroppable } from "@dnd-kit/core";
+ import { DndContext, 
+    closestCenter,
+    KeyboardSensor,
+    PointerSensor,
+    useSensor,
+    useSensors, } from "@dnd-kit/core";
+ import { arrayMove, SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy } from "@dnd-kit/sortable";
+ 
 
 export default function DashBoard(){
 
@@ -19,41 +24,94 @@ export default function DashBoard(){
     ]
     )
 
+    useEffect(function(){
+        console.log("Render")
+    }, [list])
+
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+          coordinateGetter: sortableKeyboardCoordinates,
+        })
+      );
+
     function handleFormSubmit(name){
         setList((oldList)=>[...oldList, {nameList: name, tasks:[]}])
     }
 
+    //dragEnd controla el cambio de taraeas en los mismos containers
     function handleDragEnd(event){
         const {active, over} = event
         console.log("ACTIVE : " + active.id)
         console.log("OVER :" + over.id)
+
+        const [activeContainer, activeIndex ]= findContainer(active.id, list)
+        const [overContainer, overIndex ]= findContainer(over.id, list)
+        // console.log(activeContainer)
+        // console.log(overContainer)
+        // console.log(activeIndex)
             
-        // if(active.id !== over.id){
-        //     setList( (list)=> {
-        //         const activeIndex = list.tasks.indexOf(active.id)
-        //         const overIndex = list.tasks.indexOf(over.id)
-                
-        //         return {
-        //             ...list, 
-        //             tasks: }
-        //     })
-        // }
+        if(active.id !== over.id && activeIndex === overIndex){
+            setList((oldList)=> {
+                // let arrayTask = [...oldList]
+                let arrayTask = JSON.parse(JSON.stringify(oldList))
+                let container = arrayTask[activeIndex]
+                const taskDrag = container.tasks.indexOf(active.id)
+                const taskDrop = container.tasks.indexOf(over.id)
+                container = {...container, tasks:arrayMove(container.tasks, taskDrop, taskDrag)}
+                // console.log(arrayTask)
+                // console.log(container)
+                arrayTask[activeIndex] = container
+                console.log(arrayTask)
+                return arrayTask
+            })
+        }
+            
     }
+
+    function findContainer(task, array){
+        let taskList = null
+        let name = null
+        let index = null
+        for(let i=0; i < array.length; i++){
+          taskList = array[i]
+          const lista = taskList.tasks
+          for(const t of lista){
+            if(task == t){
+              name = taskList
+              index = i
+            }
+          }
+        }
+        return [name, index]
+    }
+
+    // function moveBetweenContainers(items, activeContainer, activeIndex, ){}
    
     //  console.log(promps.listOfListTask[1].tasks)
-    const listOfList = list.map( (list)=> {
+    const listOfList = list.map( (list, index)=> {
         return(
         <ListTask 
         nameList={list.nameList}
-        listTask={list.tasks}/>)
+        listTask={list.tasks}
+        key={index}/>)
     })
+
+    //dragOver Controla el cambio en containers distintos
+    function handleDragOver(event){
+        const {active, over} = event
+
+        
+    }
     
 
     return(
         <div className="lists-container">
             <DndContext 
+            sensors={sensors}
             collisionDetection={closestCenter}
-            onDragEnd={handleDragEnd}>
+            onDragEnd={handleDragEnd}
+           >
                 <SortableContext
                 items={list}
                 strategy={verticalListSortingStrategy}>
--- src/components/ListTask.jsx
+++ src/components/ListTask.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState} from "react";
 import Task from "./Task";
 import Toggable from './Toggable'
 import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
@@ -6,17 +6,18 @@ import { useDroppable } from "@dnd-kit/core";
 
 export default function ListTask(props){
     // const list = props.listTask
-    const [list, setList] = useState(props.listTask)
+    // const [list, setList] = useState(props.listTask)
+    const list = props.listTask
 
-    
     const {setNodeRef} = useDroppable({
             id: props.nameList
         })
     
+    console.log(list)
 
-    function handleFormSubmit(name){
-        setList((oldList)=>[...oldList, name])
-    }
+    // function handleFormSubmit(name){
+    //     setList((oldList)=>[...oldList, name])
+    // }
 
     const listTask = list.map( (task) => {
         return(
@@ -27,14 +28,15 @@ export default function ListTask(props){
     })
 
     return(
-        <div ref={setNodeRef}>
+        <div ref={setNodeRef} key={props.key}>
             <h6>{props.nameList}</h6>
             <SortableContext
-                items={list}>
+                items={list}
+                id={props.nameList}>
                 {listTask}
             </SortableContext>
             <Toggable 
-            onFormSubmit={handleFormSubmit}/>
+            />
         </div>
     )
 }
